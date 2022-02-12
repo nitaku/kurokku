e36617e983af31d76c591f7d9c199214681e47c5
nitaku <matteo.abrate@gmail.com>
2022-02-12 18:04:18 +0100

    basic clock is working + SVG fit to page

---
+++ src/Clock.svelte
@@ -0,0 +1,19 @@
+<script>
+    export let date
+    export let x
+    export let y
+    const pad_time = (i) => i < 10 ? "0"+i : i
+    const blink = () => date.getMilliseconds() > 500 ? ':' : ' ' 
+</script>
+
+<text {x} {y} dy=".35em">{ pad_time(date.getHours()) + blink() +  pad_time(date.getMinutes())}</text>
+
+<style>
+    text {
+        font-size: 400px;
+        text-anchor: middle;
+        font-family: "DroidSansMono";
+        font-weight: bold;
+		fill: white;
+    }
+</style>
